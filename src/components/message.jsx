b3682eca3cefbb7121b4437f11b96d44e0d69eee
Viacheslav Kuldyaev <slavalion1@gmail.com>
2021-10-28 22:07:42 +0300

    lesson 2 finished

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import styles from '../css/message.module.css'
 
 export const Message = (props) => {
-  return <div className={styles.messagePlace}>
-            <div className = {styles.messageBody}>
-              <h5 className = {styles.author}> {props.author}</h5>
-              <div className = {styles.text}>
-                <p>{props.text}</p>
-              </div>
+  if(props.author==="ChatBot"){
+    return <div className={styles.messagePlaceRight}>
+          <div className = {styles.messageBody}>
+            <h5 className = {styles.author}> {props.author}</h5>
+            <div className = {styles.text}>
+              <p>{props.text}</p>
             </div>
           </div>
+        </div>
+  }else{
+    return <div className={styles.messagePlace}>
+          <div className = {styles.messageBody}>
+            <h5 className = {styles.authorLeft}> {props.author}</h5>
+            <div className = {styles.text}>
+              <p>{props.text}</p>
+            </div>
+          </div>
+        </div>
+  }
 }
