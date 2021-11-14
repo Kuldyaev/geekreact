ebd77368ef7c8551190331a31d60eda675cf62c9
Viacheslav Kuldyaev <slavalion1@gmail.com>
2021-11-14 12:47:49 +0300

    react-persisit added

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom';
 import { Provider} from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import storage from 'redux-persist/lib/storage'; 
 import reducer from './reducers/reducer';
 import './index.css';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const persistConfig = { key: 'chatlist', storage};
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+let store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+
+console.log(localStorage.getItem('chatlist'));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store= {store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
